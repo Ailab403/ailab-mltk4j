baa0d5a55861c7275953ecf62c011282e91791dd
superhy <superhy@superhy-PC>
2015-04-08 21:20:21 +0800

    libsvm(2015.4.8)

---
+++ src/org/mltk/libsvm/LibSVMClassifier.java
@@ -0,0 +1,11 @@
+package org.mltk.libsvm;
+
+import java.util.Map;
+
+public class LibSVMClassifier {
+
+	public Map<Integer, Integer> classifyDriver(){
+		
+		return null;
+	}
+}
--- src/org/mltk/libsvm/LibSVMTest.java
+++ src/org/mltk/libsvm/LibSVMTest.java
@@ -1,70 +1,82 @@
 package org.mltk.libsvm;
 
+import java.io.IOException;
+
 import libsvm.svm;
 import libsvm.svm_model;
 import libsvm.svm_node;
 import libsvm.svm_parameter;
 import libsvm.svm_problem;
 
-/**
- * test from web
- * 
- * @author
- * 
- *         TODO delete
- */
 public class LibSVMTest {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// 定义训练集点a{10.0, 10.0} 和 点b{-10.0, -10.0}，对应lable为{1.0, -1.0}
-		svm_node pa0 = new svm_node();
-		pa0.index = 0;
-		pa0.value = 10.0;
-		svm_node pa1 = new svm_node();
-		pa1.index = -1;
-		pa1.value = 10.0;
-		svm_node pb0 = new svm_node();
-		pb0.index = 0;
-		pb0.value = -10.0;
-		svm_node pb1 = new svm_node();
-		pb1.index = 0;
-		pb1.value = -10.0;
-		svm_node[] pa = { pa0, pa1 }; // 点a
-		svm_node[] pb = { pb0, pb1 }; // 点b
-		svm_node[][] datas = { pa, pb }; // 训练集的向量表
-		double[] lables = { 1.0, -1.0 }; // a,b 对应的lable
+		svm_problem sp = new svm_problem();
+		svm_node[][] x = new svm_node[4][2];
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 2; j++) {
+				x[i][j] = new svm_node();
+			}
+		}
+		x[0][0].index = 1;
+		x[0][0].value = 0;
+		x[0][1].index = 2;
+		x[0][1].value = 0;
 
-		// 定义svm_problem对象
-		svm_problem problem = new svm_problem();
-		problem.l = 2; // 向量个数
-		problem.x = datas; // 训练集向量表
-		problem.y = lables; // 对应的lable数组
+		x[1][0].index = 1;
+		x[1][0].value = 1;
+		x[1][1].index = 2;
+		x[1][1].value = 1;
 
-		// 定义svm_parameter对象
-		svm_parameter param = new svm_parameter();
-		param.svm_type = svm_parameter.C_SVC;
-		param.kernel_type = svm_parameter.LINEAR;
-		param.cache_size = 100;
-		param.eps = 0.00001;
-		param.C = 1;
+		x[2][0].index = 1;
+		x[2][0].value = 0;
+		x[2][1].index = 2;
+		x[2][1].value = 1;
 
-		// 训练SVM分类模型
-		System.out.println(svm.svm_check_parameter(problem, param)); // 如果参数没有问题，则svm.svm_check_parameter()函数返回null,否则返回error描述。
-		svm_model model = svm.svm_train(problem, param); // svm.svm_train()训练出SVM分类模型
+		x[3][0].index = 1;
+		x[3][0].value = 1;
+		x[3][1].value = 0;
+		x[3][1].index = 2;
 
-		// 定义测试数据点c
-		svm_node pc0 = new svm_node();
-		pc0.index = 0;
-		pc0.value = -0.1;
-		svm_node pc1 = new svm_node();
-		pc1.index = -1;
-		pc1.value = 0.0;
-		svm_node[] pc = { pc0, pc1 };
+		double[] labels = new double[] { -1, -1, 1, 1 };
+		sp.x = x;
+		sp.y = labels;
+		sp.l = 4;
+		svm_parameter prm = new svm_parameter();
+		prm.svm_type = svm_parameter.C_SVC;
+		prm.kernel_type = svm_parameter.RBF;
+		prm.C = 1000;
+		prm.eps = 0.0000001;
+		prm.gamma = 10;
+		prm.probability = 1;
+		prm.cache_size = 1024;
+		/*
+		 * svm_check_parameter 参数可行返回null，否则返回错误信息
+		 */
+		System.out.println("Param Check "
+				+ (svm.svm_check_parameter(sp, prm) == null));
+		svm_model model = svm.svm_train(sp, prm); // 训练分类
+		try {
+			svm.svm_save_model("svm_model_file", model);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-		// 预测测试数据的lable
-		System.out.println(svm.svm_predict(model, pc));
+		try {
+			svm.svm_load_model("svm_model_file");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		svm_node[] test = new svm_node[] { new svm_node(), new svm_node() };
+		test[0].index = 1;
+		test[0].value = 0;
+		test[1].index = 2;
+		test[1].value = 0;
+		double[] l = new double[2];
+		double result_prob = svm.svm_predict_probability(model, test, l); // 测试1，带预测概率的分类测试
+		double result_normal = svm.svm_predict(model, test); // 测试2 不带概率的分类测试
+		System.out.println("Result with prob " + result_prob);
+		System.out.println("Result normal " + result_normal);
+		System.out.println("Probability " + l[0] + "\t" + l[1]);
 	}
 }
