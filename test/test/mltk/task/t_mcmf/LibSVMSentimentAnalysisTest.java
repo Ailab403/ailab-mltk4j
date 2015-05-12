19c93653c0921e9dc033570d8a1722258926fe70
superhy <superhy@superhy-PC>
2015-05-12 14:28:10 +0800

    classify_evaluaton(2015.5.12)

--- test/test/mltk/task/t_mcmf/LibSVMSentimentAnalysisTest.java
+++ test/test/mltk/task/t_mcmf/LibSVMSentimentAnalysisTest.java
@@ -2,6 +2,7 @@ package test.mltk.task.t_mcmf;
 
 import java.util.List;
 
+import org.ansj.util.ClassifyEvaluation;
 import org.mltk.libsvm.model.ClassifyRes;
 import org.mltk.task.t_mcmf.LibSVMSentimentAnalysis;
 
@@ -23,15 +24,22 @@ public class LibSVMSentimentAnalysisTest {
 				libsvmModelPath);
 		List<ClassifyRes> classifyResList = libSVMSentimentAnalysis
 				.libsvmSentimentAnalysis(testModelPath, libsvmModelPath);
+
+		/*
+		 * svm_type c_svc kernel_type rbf gamma 10.0 nr_class 2 total_sv 183 rho
+		 * -0.3359877695622573 label 1 0 probA -2.988586914322829 probB
+		 * 0.06357078309697942 nr_sv 97 86 SV
+		 */
+
 		int probNum = 0, normNum = 0;
 		int textNum = 0;
 		for (ClassifyRes classifyRes : classifyResList) {
-			if ((classifyRes.getProbilityRes() == 0.0 && textNum >= TestCorpusParam.POS_TAG)
-					|| (classifyRes.getProbilityRes() == 1.0 && textNum < TestCorpusParam.NEG_TAG)) {
+			if ((classifyRes.getProbilityRes() == 0.0 && textNum >= ClassifyEvaluation.POS_TAG)
+					|| (classifyRes.getProbilityRes() == 1.0 && textNum < ClassifyEvaluation.NEG_TAG)) {
 				probNum++;
 			}
-			if ((classifyRes.getNormalRes() == 0.0 && textNum >= TestCorpusParam.POS_TAG)
-					|| (classifyRes.getNormalRes() == 1.0 && textNum < TestCorpusParam.NEG_TAG)) {
+			if ((classifyRes.getNormalRes() == 0.0 && textNum >= ClassifyEvaluation.POS_TAG)
+					|| (classifyRes.getNormalRes() == 1.0 && textNum < ClassifyEvaluation.NEG_TAG)) {
 				normNum++;
 			}
 			textNum++;
--- test/test/mltk/task/t_mcmf/TestCorpusParam.java
+++
@@ -1,7 +0,0 @@
-package test.mltk.task.t_mcmf;
-
-public class TestCorpusParam {
-
-	public static int POS_TAG = 48;
-	public static int NEG_TAG = 152;
-}
