324a21444f4f26a04415e2dce328a85a44908e49
daidan <daidan@aca80323.ipt.aol.com>
2016-03-26 17:29:28 +0800

    name_find_tester(2016.3.26)

--- src/org/mltk/openNLP/NameEntityFinder.java
+++ src/org/mltk/openNLP/NameEntityFinder.java
@@ -1,5 +1,38 @@
 package org.mltk.openNLP;
 
+import opennlp.tools.namefind.NameFinderME;
+
 public class NameEntityFinder {
+
+	// 默认参数
+	private double probThreshold = 0.6;
+
+	private String modelPath;
+
+	public NameEntityFinder() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NameEntityFinder(String modelPath) {
+		super();
+		this.modelPath = modelPath;
+	}
+
+	public NameEntityFinder(double probThreshold, String modelPath) {
+		super();
+		this.probThreshold = probThreshold;
+		this.modelPath = modelPath;
+	}
+
+	/**
+	 * 生成NameFinder
+	 * 
+	 * @return
+	 */
+	public NameFinderME prodNameFinder() {
+		
+		return null;
+	}
 
 }
